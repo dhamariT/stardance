bcace06b3b849c8a0f30f251a4acf4ccba3cfa95
Kartikey Chauhan <me@cskartikey.dev>
2026-05-08 20:50:18 +0530

    FUCK PART 2

--- db/migrate/20260507052538_create_user_preference.rb
+++ db/migrate/20260507052538_create_user_preference.rb
@@ -1,6 +1,6 @@
 class CreateUserPreference < ActiveRecord::Migration[8.1]
   def change
-    create_table :user_preference do |t|
+    create_table :user_preferences do |t|
       t.references :user, null: false, foreign_key: true, index: { unique: true }
       t.boolean :send_votes_to_slack, null: false, default: false
       t.boolean :leaderboard_optin, null: false, default: false
@@ -14,6 +14,6 @@ class CreateUserPreference < ActiveRecord::Migration[8.1]
       t.timestamps
     end
 
-    add_index :user_preference, :leaderboard_optin
+    add_index :user_preferences, :leaderboard_optin
   end
 end
